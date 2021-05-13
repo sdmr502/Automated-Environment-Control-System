67c14ae223bd055422d4ed62083f6943d47aeaae
Sam Robinson <53836799+sdmr502@users.noreply.github.com>
2021-05-13 19:58:53 +0100

    Update startEnviromentv3.py

--- startEnviromentv3.py
+++ startEnviromentv3.py
@@ -11,14 +11,18 @@ import aqi
 import urllib.request, urllib.parse
 import mariadb
 from meteocalc import Temp, dew_point, heat_index, wind_chill, feels_like
+import requests
+import collections
+import numpy as np
+
 
 
 # Connect to MariaDB Platform
 try:
     conn = mariadb.connect(
-        user="#",
-        password="#",
-        host="#",
+        user="",
+        password="",
+        host="",
         port=3306,
         database="climatesys"
     )
@@ -121,7 +125,7 @@ def withinThreshold(threshold, target, actual):
 
 def setDHTEnvironmentData(sensorPin, sensorType,):
     humidity, temperature_c = Adafruit_DHT.read_retry(sensorType, sensorPin)
-    if(temperature_c is not None and -20 <= temperature_c <= 100 and humidity is not None and 0 <= humidity <= 100):
+    if(temperature_c is not None and -20 <= temperature_c <= 100 and humidity is not None and 1 <= humidity <= 100):
         humidity = round(humidity,2)
         temperature_c = round(temperature_c,2)
         temp_c_meteocalc = Temp(temperature_c, 'c')
@@ -150,6 +154,7 @@ def numConcat(num1, num2):
      num1 += num2 
   
      return num1 
+
 def balancer(actual,targetMin, targetMax):
     #The function allows us to simplify the results to determine the best method more dynamically
     if(targetMin <= actual <= targetMax):
@@ -159,29 +164,101 @@ def balancer(actual,targetMin, targetMax):
     elif(actual < targetMin and actual < targetMax):
         return 3
     elif(targetMin > actual):
-        return 2
-    elif(targetMax < actual):
         return 4
+    elif(targetMax < actual):
+        return 5
     else:
-        return 0        
-#def correctBalance(option):
+        return 0
+
+def predictOutcome(use, json):
+    if(use is "TempPredict"):
+        response = requests.post('https://*****.azurewebsites.net/api/'+ use, json=json)
+    elif(use is "HumPredict"):
+        response = requests.post('https://****.azurewebsites.net/api/'+ use, json=json)
+        #response = requests.post('https://*****.azurewebsites.net/api/Predict', json=json)
+    return(response.json())
+
+def buildRequestData(env, use, a, b, c):
+    if(use is "TempPredict"):
+        return {"MainHum": env["room"]["humidity"],"IntakeTemp": env["intake"]["temp"],"IntakeHum": env["intake"]["humidity"],"OuttakeTemp": env["outtake"]["temp"],"OuttakeHum": env["outtake"]["humidity"],"MainLight":a,"IntakeFan":b,"OuttakeFan":c}
+    elif(use is "HumPredict"):
+        return {"MainTemp": env["room"]["temp"],"IntakeTemp": env["intake"]["temp"],"IntakeHum": env["intake"]["humidity"],"OuttakeTemp": env["outtake"]["temp"],"OuttakeHum": env["outtake"]["humidity"],"MainLight":a,"IntakeFan":b,"OuttakeFan":c}
+
+def closest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+def correctBalance(cur, env, use):
+    est = []
+    pre = []
     #we are going to ignore the first digit in our option.
+    cur.execute("SELECT * FROM `matrix` WHERE `Lighting`=?", (env["outputs"]["lighting"]["status"],))
+    for id, Lighting, IntakeFan,OuttakeFan in cur:
+        result = predictOutcome(use, buildRequestData(environment, use, Lighting, IntakeFan, OuttakeFan))
+        print(f"\033[1;36;40m Matrix {id} -  IF: {IntakeFan}, OF: {OuttakeFan} - Prediction: {round(result['prectiction'],2)} \n")
+      
+        est.append ({ "id" : id, "prediction": round(result['prectiction'],2), "IntakeFan": IntakeFan,"OuttakeFan": OuttakeFan})
+      
+        pre.append(round(result['prectiction'],2))
+
+    est.sort(key=lambda x: x.get('prediction'))
+
+    option = [int(x) for x in str(env['option'])]
     
-    
-    
+    print(option , "\n")
+    if(use is 'TempPredict'):
+        #Add 1 to method which we can use the cloest method
+        if(option[0] is 1):
+            index = 5
+            #print({k: v for k, v in est if v['prediction'] == })
+        elif(option[0] is 2):
+            index = est[0]['id']
+        elif(option[0] is 3):
+            index = est[-1]['id']
+    elif(use is 'HumPredict'):
+          #Add 1 to method which we can use the cloest method
+        print(option[1])
+        if(option[1] is 1 and option[4] is not 2):
+            index = 8
+            #print({k: v for k, v in est if v['prediction'] == })
+        elif(option[1] is 2 and option[0] is not 3):
+            index = est[3]['id']
+        elif(option[1] is 3 or option[4] is 2 or option[0] is 3):
+            index = est[0]['id']
+
+    if(index):
+        cur.execute("SELECT * FROM `matrix` WHERE `id`=?", (int(index),))
+        for id, Lighting, IntakeFan,OuttakeFan in cur:
+            print(f"\033[1;32;40m Matrix {id} Wins -  IF: {IntakeFan}, OF: {OuttakeFan} \n")
+            if(IntakeFan):
+                setRelay(SIDELEFT_FAN, "start")
+            else:
+                setRelay(SIDELEFT_FAN, "stop")
+
+            if(OuttakeFan):
+                setRelay(SIDERIGHT_FAN, "start")
+            else:
+                setRelay(SIDERIGHT_FAN, "stop")
+        
+def GetKey(dictA,val):
+   for key, value in dictA.items():
+      if val == value:
+         return key
+      return "key doesn't exist"        
+
 
 TARGET_TEMP = 27
-TARGET_HU = 47
+TARGET_HU = 45
 
 TEMPERATURE_THRESHOLD_PERCENTAGE = 15
 HUMIDITY_THRESHOLD_PERCENTAGE = 5
 
 
 
-        
 environment = {
     "thresholds":{},
-    "moist": checkMoisture(SOIL_SENSOR_1),
+    "moist": int(checkMoisture(SOIL_SENSOR_1)),
     "intake": {},
     "outtake": {},
     "room": {},
@@ -189,7 +266,8 @@ environment = {
         "lighting": {'status' : 0},
         "intakeFan": {'status' : 0},
         "outtakeFan": {'status' : 0}, 
-        }
+        },
+    "option": 0
     }
 
 while True:
@@ -198,18 +276,15 @@ while True:
         localtime = time.asctime( time.localtime(time.time()) )
         print("\033[1;36;40m Local current time :", localtime ,"\n")
         
-        if(is_between(timestamp, ("08:00", "01:59"))):
+        if(is_between(timestamp, ("22:00", "10:05"))):
             print("\033[1;33;40m Light should be set ON \n")
             setRelay(MAIN_LIGHTING, "start")
         else:
             print("\033[1;34;40m Light should be set OFF \n")
             setRelay(MAIN_LIGHTING, "stop")
-            cleanAirCycle()
             
-        
         cur.execute("SELECT AVG(mainDewPoint) FROM env WHERE timestamp > NOW() - INTERVAL 48 HOUR")
         riskDewPoint = cur.fetchone()
-        
         
         environment["thresholds"] = {
             "temp": getThreshold(TEMPERATURE_THRESHOLD_PERCENTAGE, TARGET_TEMP),
@@ -235,7 +310,6 @@ while True:
             checkTemperatureThreshold = withinThreshold(environment["thresholds"]['temp'], TARGET_TEMP, environment["room"]['temp'])
             checkHumidityThreshold = withinThreshold(environment["thresholds"]['humidity'], TARGET_HU, environment["room"]['humidity'])
         
-            print(environment)
             balancerTemp = balancer(environment["room"]['temp'],environment["thresholds"]['triggers']['tempMin'], environment["thresholds"]['triggers']['tempMax'])
             balancerHumid = balancer(environment["room"]['humidity'],environment["thresholds"]['triggers']['humidityMin'], environment["thresholds"]['triggers']['humidityMax'])
             balancerIntakeTemp = balancer(environment["intake"]['temp'],environment["thresholds"]['triggers']['tempMin'], environment["thresholds"]['triggers']['tempMax'])
@@ -243,36 +317,38 @@ while True:
             balancerOuttakeTemp = balancer(environment["outtake"]['temp'],environment["thresholds"]['triggers']['tempMin'], environment["thresholds"]['triggers']['tempMax'])
             balancerOuttakeHumid = balancer(environment["outtake"]['humidity'],environment["thresholds"]['triggers']['humidityMin'], environment["thresholds"]['triggers']['humidityMax'])
             
-            globalBalancer = numConcat(balancerTemp,numConcat(balancerHumid,numConcat(balancerIntakeTemp,numConcat(balancerIntakeHumid,numConcat(balancerOuttakeTemp,balancerOuttakeHumid)))))
-            
+            environment["option"] = numConcat(balancerTemp,numConcat(balancerHumid,numConcat(balancerIntakeTemp,numConcat(balancerIntakeHumid,numConcat(balancerOuttakeTemp,balancerOuttakeHumid)))))
             if checkTemperatureThreshold and checkHumidityThreshold:
-                print("Relax Everything is Good :)")
-                option = numConcat(1,globalBalancer)
+                version = "NoPrediction"
+                print("Relax Everything is Good :) \n")
+                environment["option"] = numConcat(1,environment["option"])
                 setRelay(SIDERIGHT_FAN, "stop")
                 setRelay(SIDELEFT_FAN, "stop")
             elif (checkHumidityThreshold is 0 and checkTemperatureThreshold):
-                option = numConcat(2,globalBalancer)
-                setRelay(SIDERIGHT_FAN, "start")
-                setRelay(SIDELEFT_FAN, "stop")
-                print("Humidity Correction Required")
+                version = "HumMLModel"
+                correctBalance(cur, environment, "HumPredict")
+                environment["option"] = numConcat(2,environment["option"])
+                print("Humidity Correction Required \n")
             elif (checkHumidityThreshold and checkTemperatureThreshold is 0):
-                option = numConcat(3,globalBalancer)
-                print("Temperature Correction Required")
-                setRelay(SIDERIGHT_FAN, "stop")
-                setRelay(SIDELEFT_FAN, "start")
+                version = "MLModel"
+                correctBalance(cur, environment, "TempPredict")
+                environment["option"] = numConcat(3,environment["option"])
+                print("Temperature Correction Required \n")
             else:
-                option = numConcat(4,globalBalancer)
-                print("Enviroment Unstable")
-                setRelay(SIDERIGHT_FAN, "start")
-                setRelay(SIDELEFT_FAN, "start")
+                print("Enviroment Unstable \n")
+                version = "MLModel"
+                correctBalance(cur, environment, "TempPredict")
+                environment["option"] = numConcat(4,environment["option"])
+                #setRelay(SIDERIGHT_FAN, "start")
+                #setRelay(SIDELEFT_FAN, "start")
             cur.execute(
-                "INSERT INTO env (action,mainTemp,mainHum,mainDewPoint,mainHeatIndex,intakeTemp,intakeHum,intakeDewPoint,intakeHeatIndex,outtakeTemp,outtakeHum,outtakeDewPoint,outtakeHeatIndex,MoistureSensor1,mainLight,intakeFan,outtakeFan,option,targetTemp,targetHum,tempThreshold,humThreshold,tempMax,tempMin,humMax,humMin) VALUES (?, ?, ? ,? ,? ,? ,? ,? ,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                ("self", environment["room"]['temp'], environment["room"]['humidity'], environment["room"]['dewPoint'], environment["room"]['heatIndex'],
+                "INSERT INTO env (version,mainTemp,mainHum,mainDewPoint,mainHeatIndex,intakeTemp,intakeHum,intakeDewPoint,intakeHeatIndex,outtakeTemp,outtakeHum,outtakeDewPoint,outtakeHeatIndex,MoistureSensor1,mainLight,intakeFan,outtakeFan,option,targetTemp,targetHum,tempThreshold,humThreshold,tempMax,tempMin,humMax,humMin) VALUES (?, ?, ? ,? ,? ,? ,? ,? ,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                (version, environment["room"]['temp'], environment["room"]['humidity'], environment["room"]['dewPoint'], environment["room"]['heatIndex'],
                             environment["intake"]['temp'], environment["intake"]['humidity'], environment["intake"]['dewPoint'], environment["intake"]['heatIndex'],
                             environment["outtake"]['temp'], environment["outtake"]['humidity'], environment["outtake"]['dewPoint'], environment["outtake"]['heatIndex'],
                             environment["moist"],
                             environment["outputs"]["lighting"]["status"], environment["outputs"]["intakeFan"]["status"], environment["outputs"]["outtakeFan"]["status"],
-                            option, TARGET_TEMP, TARGET_HU, environment["thresholds"]["temp"], environment["thresholds"]["humidity"],
+                            environment["option"], TARGET_TEMP, TARGET_HU, environment["thresholds"]["temp"], environment["thresholds"]["humidity"],
                             environment["thresholds"]['triggers']["tempMax"],environment["thresholds"]['triggers']["tempMin"],environment["thresholds"]['triggers']["humidityMax"],environment["thresholds"]['triggers']["humidityMin"]     
                  ))
         
